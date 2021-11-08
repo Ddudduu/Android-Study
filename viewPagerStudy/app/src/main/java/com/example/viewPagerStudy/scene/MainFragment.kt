fbc9bd24929affe7d6879ef68d2581f3f0cfc92b
임지원 <senta2006@naver.com>
2021-11-08 14:07:01 +0900

    viewPager 콜백리스너 등록

--- viewPagerStudy/app/src/main/java/com/example/viewPagerStudy/scene/MainFragment.kt
+++ viewPagerStudy/app/src/main/java/com/example/viewPagerStudy/scene/MainFragment.kt
@@ -1,13 +1,16 @@
 package com.example.viewPagerStudy.scene
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.viewPagerStudy.R
 import com.example.viewPagerStudy.databinding.MainFragmentBinding
 import com.example.viewPagerStudy.model.Profile
+import timber.log.Timber
 
 class MainFragment : Fragment() {
   private lateinit var binding: MainFragmentBinding
@@ -33,5 +36,25 @@ class MainFragment : Fragment() {
     mainViewAdapter = MainViewAdapter()
     mainViewAdapter.submitList(profileList)
     binding.viewPager.adapter = mainViewAdapter
+    binding.viewPager.registerOnPageChangeCallback(pageChangeCallback)
+  }
+
+  private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+    override fun onPageScrollStateChanged(state: Int) {
+      super.onPageScrollStateChanged(state)
+      Log.w("$$$0 ", "$state")
+    }
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+      super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+      Log.w("$$$1 ", "$position")
+      Log.w("$$$2 ", "$positionOffset")
+      Log.w("$$$3 ", "$positionOffsetPixels")
+    }
+
+    override fun onPageSelected(position: Int) {
+      super.onPageSelected(position)
+      Log.w("$$$4 ", "$position")
+    }
   }
 }
